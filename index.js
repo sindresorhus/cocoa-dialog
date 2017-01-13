275e7f0cf624befd5803cff3a7fd0da60f097f54
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-13 16:21:58 +0700

    Bump `execa`

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ module.exports = (type, opts) => {
 
 	const args = dargs(opts, {useEquals: false});
 
-	return execa(bin, [type].concat(args))
-		// TODO: use execa `reject: false` option
-		// it exits with code 1 on success, ugh
-		.catch(err => err.stdout.replace(/\r?\n$/, ''));
+	return execa.stdout(bin, [type].concat(args), {
+		// It exits with code 1 on success, ugh...
+		reject: false
+	}).then(stdout => stdout.replace(/\r?\n$/, ''));
 };
